916a2e2483c1ff2afea7e7cb12c1654f3f5d338b
Vagner do Carmo <vluzrmos@gmail.com>
2021-05-23 20:21:09 -0300

    Fix portal cache; add portal to post struct; add portal color

--- data/data.go
+++ data/data.go
@@ -18,6 +18,7 @@ type Portal struct {
 	Id               uint   `json:"id,omitempty" bson:"_id,omitempty"`
 	Name             string `json:"name" bson:"name"`
 	Description      string `json:"description,omitempty" bson:"description,omitempty"`
+	Color            string `json:"color,omitempty" bson:"color,omitempty"`
 	Url              string `json:"url" bson:"url"`
 	HeadlineSelector string `json:"-" bson:"-"`
 }
@@ -28,6 +29,7 @@ type Post struct {
 	Title     string    `json:"title" bson:"title"`
 	Url       string    `json:"url" bson:"url"`
 	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+	Portal    *Portal   `json:"portal,omitempty" bson:"-"`
 }
 
 var DefaultPerPage int64 = 25
@@ -37,65 +39,76 @@ var Portals = []Portal{
 		Id:               1,
 		Name:             "Globo",
 		Url:              "https://globo.com",
+		Color:            "blue",
 		HeadlineSelector: "body > section.highlight-container.hui-container.hui-spacing > div.highlight-container__left-area.highlight-main > div > a > h3",
 	},
 	{
 		Id:               2,
 		Name:             "UOL",
+		Color:            "brown",
 		Url:              "https://uol.com.br",
 		HeadlineSelector: "#corpo > div:nth-child(1) > div > div.topo-hibrido-central.centraliza.clearfix.bloco-editorial-topo-1 > div.topo-hibrido-hardnews > div.topo-hibrido-hardnews-destaque > div.mod-hibrido-manchete.area-default.manchete-editorial > a > h1",
 	},
 	{
 		Id:               3,
 		Name:             "Folha",
+		Color:            "black",
 		Url:              "https://www.folha.uol.com.br",
 		HeadlineSelector: ".c-main-headline__title",
 	},
 	{
 		Id:               4,
 		Name:             "Veja",
+		Color:            "red",
 		Url:              "https://veja.com.br",
 		HeadlineSelector: "body > main > section.block.hard-news.light > div > div > div.col-s-12.col-l-9 > div.card.d > a:nth-child(1) > h2",
 	},
 	{
 		Id:               5,
 		Name:             "Estadão",
+		Color:            "blue",
 		Url:              "https://www.estadao.com.br/",
 		HeadlineSelector: "#wrapper > section.breaking-news > div > div > div:nth-child(1) > article > div > div > div > div.intro > a > h3",
 	},
 	{
 		Id:               6,
 		Name:             "Terra",
+		Color:            "orange",
 		Url:              "https://www.terra.com.br/",
 		HeadlineSelector: "div.card-premium__left > h2 > a",
 	},
 	{
 		Id:               7,
 		Name:             "Metro1",
+		Color:            "yellow",
 		Url:              "https://www.metro1.com.br/",
 		HeadlineSelector: "body > div > main section:nth-child(1) article div a",
 	},
 	{
 		Id:               8,
 		Name:             "Exame",
+		Color:            "red",
 		Url:              "https://exame.com",
 		HeadlineSelector: "#abril_home_box_widget-101 > div.widget-box.widget-home-box.widget-box- > div.hide_thumb.widget-home-box-list-item.type-post.item-size-g > div > a.widget-home-box-list-item-title > h2",
 	},
 	{
 		Id:               9,
 		Name:             "Correio",
+		Color:            "red",
 		Url:              "https://correio24horas.com.br",
 		HeadlineSelector: "#CW9220 > div.destaque-responsivo__container-info > div > a",
 	},
 	{
 		Id:               10,
 		Name:             "Metrópoles",
+		Color:            "yellow",
 		Url:              "https://www.metropoles.com",
 		HeadlineSelector: "#m-main > section.m-top-news > div > div > div.column.is-three-quarters-widescreen.is-full > article > div:nth-child(2) > div > h2 > a",
 	},
 	{
 		Id:               11,
 		Name:             "BNews",
+		Color:            "red",
 		Url:              "https://www.bnews.com.br/",
 		HeadlineSelector: "h3.title a",
 	},
@@ -220,16 +233,30 @@ func FetchHeadlines(filters interface{}, findOptions *options.FindOptions) *[]Po
 
 	results.All(ctx, &posts)
 
+	for i := range posts {
+		posts[i].Portal = FindPortalById(&Portals, posts[i].PortalId)
+	}
+
 	return &posts
 }
 
-func FindPortalById(portals *[]Portal, id uint) *Portal {
-	for _, portal := range *portals {
+var portalsCache = make(map[uint]*Portal)
 
+func FindPortalById(portals *[]Portal, id uint) *Portal {
+	if portal, found := portalsCache[id]; found {
+		return portal
+	}
+
+	for _, portal := range *portals {
 		if portal.Id == id {
+			portalsCache[id] = &portal
+
 			return &portal
 		}
 	}
 
-	return &Portal{}
+	portal := &Portal{}
+	portalsCache[id] = portal
+
+	return portal
 }
